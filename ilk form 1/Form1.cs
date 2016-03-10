59e61354f6eba7f4955e74faa18e58e11d46ba0c
hashuscet <hasan.6464@outlook.com>
2016-03-10 09:10:30 +0200

    Paragraphs in the body

---
+++ ilk form 1/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ilk_form_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listBox1.Items.Add("Uşak");
+            listBox1.Items.Add("denizli");
+            listBox1.Items.Add("manisa");
+        }
+    }
+}
